c921a801f6167e6fa9ea5e98b4866a69b4beb7bf
Pierre <pierre6richard7@gmail.com>
2024-04-29 17:53:03 +0200

    fix: readmap

--- Moteur/src/mapreader.hpp
+++ Moteur/src/mapreader.hpp
@@ -25,9 +25,22 @@ vector<vector<int>> readmap(const std::string &path){
         {
             ligne.push_back((int)line[i]-'0');
         }
-        result.push_back(ligne);
+        temp.push_back(ligne);
     } 
-  
+    result.resize(temp.size());
+    for (int i = 0; i < temp[0].size(); i++)
+    {
+        result.push_back(vector<int>());
+    }
+    
+    for (int i = 0; i < temp.size(); i++)
+    {
+        for (int j = temp[i].size()-1; j >= 0; j--)
+        {
+            result[i].push_back(temp[j][i]);
+        }
+    }
+
     inputFile.close(); 
 
     return result;
